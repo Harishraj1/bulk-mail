07dea2b3e0ce753435a716b73f5d22fd75eddc5b
HARISHRAJ D <harishrajd6@gmail.com>
2024-08-20 11:48:13 +0530

    first commit

--- backend/index.js
+++ backend/index.js
@@ -1,72 +1,67 @@
-const express = require('express')
-const cors = require('cors')
+const express = require("express")
+const cors = require("cors")
 const nodemailer = require("nodemailer");
+const mongoose = require("mongoose")
+
 const app = express()
-const mongooes = require('mongoose')
 
 app.use(cors())
 app.use(express.json())
 
-mongooes.connect("mongodb+srv://harishrajd24:123@cluster0.xa5qtja.mongodb.net/passkey?retryWrites=true&w=majority").then(function(){
-    console.log("Connected to DB")
+mongoose.connect("mongodb://127.0.0.1:27017/passkey").then(function(){
+    console.log("db connected")
 }).catch(function(){
-    console.log("connection failed")
+    console.log("db failed...")
 })
 
-const credential = mongooes.model("credential",{},"bulkmail")
+const credential = mongoose.model("credential",{},"bulkmail")
 
-
-app.post("/mail", function (req, res){
+app.post("/mail",function(req,res){
     var msg = req.body.msg
-    var emailList = req.body.emailList
+    var email = req.body.emailList
+
     credential.find().then(function(data){
+
         const transporter = nodemailer.createTransport({
-            service: "gmail",
+            service:"gmail",
             auth: {
-                // TODO: replace `user` and `pass` values from <https://forwardemail.net>
-                user: data[0].toJSON().user,
-                pass: data[0].toJSON().pass,
-            },
+            user: data[0].toJSON().user,
+            pass: data[0].toJSON().pass,
+          },
         });
     
         new Promise(async function(resolve,reject){
-            try {
-                for (var i = 0; i < emailList.length; i++)
-                {
+    
+            try{
+                for(var i=0; i < email.length; i++)
+                    {
                     await transporter.sendMail(
                         {
-                            from: "harishrajd6@gmail.com",
-                            to: emailList[i],
-                            subject: "samplee4",
-                            text: msg
+                            from:"harishrajd6@gmail.com",
+                            to:email[i],
+                            subject:"hi",
+                            text:msg
                         }
                     )
-                    console.log("email send to:"+emailList[i])
                 }
                 resolve("success")
             }
-            catch(error)
-            {
-                reject("failed")
+            catch(error){
+                reject("Failed")
             }
-        }).then(function()
-        {
+        }).then(function(){
             res.send(true)
-        })
-        .catch(function()
-        {
+        }).catch(function(){
             res.send(false)
         })
     
-    }).catch(function()
-    {
-        console.log("errorr")
+    }).catch(function(error){
+        console.log(error)
     })
 
 })
 
 
-
-app.listen(5000, function () {
-    console.log("server started...")
+app.listen(5000,function(){
+    console.log("server started..!")
 })
